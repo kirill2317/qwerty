832eed29ad320578ecc8b9479901f072ad35216c
kirill2317 <kp4354431@gmail.com>
2024-11-23 12:28:55 +0300

    Add files via upload

---
+++ qwerty.py
@@ -0,0 +1,16 @@
+number_of_exercises = 12
+print(type(number_of_exercises))
+print(number_of_exercises)
+number_of_hours = 4
+print(number_of_hours)
+cours_name = "informatika"
+print(cours_name)
+time_for_one_task = number_of_hours/number_of_exercises
+print(time_for_one_task)
+print(number_of_exercises)
+print('number_of_exercises = 12,number_of_hours = 4,cours_name = "informatika"')
+print('Название курса',cours_name, 'количество заданий',number_of_exercises, 'количество часов',number_of_hours,'время на одно задание',time_for_one_task )
+
+
+
+
